3bed964958f2e481621398a27eaf679d8d2fe866
jackdoyle5225 <jackdoyle12345@gmail.com>
2022-02-20 21:31:58 +0000

    new recipes thank u ciara

--- sidebar.php
+++ sidebar.php
@@ -5,13 +5,20 @@
         <a href="">Smash Burgers</a><br>
         <a href="">Chicken Parmesan pasta</a><br>
         <a href="">Quesadillas</a><br>
+        <a href="">McMuffins</a><br>
+        <a href="">Garlic Butter Steak</a><br>
+        <a href="">Breaded Chicken Burger</a><br>
         <p><b>Desserts</b></p>
         <a href="recipes/baileys_cheesecake.php">Baileys Cheesecake</a><br>
         <a href="recipes/lemon_cheesecake.php">Lemon Cheesecake</a><br>
         <a href="recipes/brownies.php">Ciara's Brownies</a><br>
+        <a href="">Cinnamon Rolls</a><br>
+        <a href="">Lemon Drizzle Cake</a><br>
+        <a href="">Cheesecake Cookie Sandwiches</a><br>
         <p><b>Sides</b></p>
         <a href="">Air Fryer Chips</a><br>
         <a href="">Halloumi Fries</a><br>
         <a href="">Guacemole</a><br>
         <a href="recipes/burger_sauce.php">Burger Sauce</a><br>
+        <a href="">EZ Cheese Sauce</a><br>
     </div>
