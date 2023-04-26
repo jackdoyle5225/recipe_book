9ce849fd0932143db6eb96ae731fb839f6aca0b3
jack <jackdoyle12345@gmail.com>
2023-04-26 20:44:40 +0100

    working?

--- parsedowntest.php
+++ parsedowntest.php
@@ -1,9 +1,24 @@
 <?php include 'Parsedown.php'; ?>
 
 <?php
+    <!DOCTYPE html>
+    <html>
     $parsedown = new Parsedown();
 
     $file = fopen('recipes/burger_sauce.md', 'r');
     $line = file('template.md');
-    echo $parsedown->text(fread($file, filesize('template.md')));
+
+    <head>
+    <title>burger_sauce</title>
+    <link rel="stylesheet" href="../style.css">
+    </head>
+    
+    <body>
+    <div class="main">
+        echo $parsedown->text(fread($file, filesize('template.md')));
+    </div>
+
+    <?php require '../footer.php'; ?>
+    </body>
+    </html>
 ?>
