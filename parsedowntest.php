4a1557357af87a0b232aa1f528abfc6e4ad5f7cb
jack <jackdoyle12345@gmail.com>
2023-04-26 20:50:15 +0100

    please

--- parsedowntest.php
+++ parsedowntest.php
@@ -1,24 +1,25 @@
 <?php include 'Parsedown.php'; ?>
 
+
+<!DOCTYPE html>
+<html>
 <?php
-    <!DOCTYPE html>
-    <html>
     $parsedown = new Parsedown();
 
     $file = fopen('recipes/burger_sauce.md', 'r');
     $line = file('template.md');
-
-    <head>
-    <title>burger_sauce</title>
-    <link rel="stylesheet" href="../style.css">
-    </head>
-    
-    <body>
-    <div class="main">
-        echo $parsedown->text(fread($file, filesize('template.md')));
-    </div>
-
-    <?php require '../footer.php'; ?>
-    </body>
-    </html>
 ?>
+
+<head>
+<title>burger_sauce</title>
+<link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+<div class="main">
+    <?php echo $parsedown->text(fread($file, filesize('template.md'))); ?>
+</div>
+
+<?php require '../footer.php'; ?>
+</body>
+</html>
