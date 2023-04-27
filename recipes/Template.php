6ca244ee59052fb18e8b7bb070b8f717c9179d7e
jack <jackdoyle12345@gmail.com>
2023-04-27 16:57:09 +0100

    rer

--- recipes/Template.php
+++ recipes/Template.php
@@ -8,7 +8,35 @@
 <body>
 <!-- recipe -->
 <div class="main">
+    <h1 id="foodname">foodname<h1>
+    <p><img src="tony_smiling.jpg" width="600" height="600" alt="food"></p>
 
+    <h3 id="ingredients">Ingredients</h3>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+    </ul>
+    <p>portion sizes</p>
+
+    <h3 id="directions">Directions</h3>
+    <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+    </ol>
 </div>
 
 <!-- footer -->
