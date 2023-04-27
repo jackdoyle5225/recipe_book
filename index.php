756d8ee6c184104f67e928eb8ac79c5f9801d2eb
jack <jackdoyle12345@gmail.com>
2023-04-27 14:53:09 +0100

    card categories

--- index.php
+++ index.php
@@ -16,24 +16,75 @@
         <h1>Recipe Book</h1>
         <p>This website is a collection of recipes I use to keep myself fed :)</p>
 
+        <!-- card display for recipes -->
         <div class="cards">
-            <div class="row">
-            <div class="column">
-                <div class="card">
-                <img src="images/pizza.jpg" width="60" height="60" alt="Pizza">
-                </div>
-            </div>
-            <div class="column">
-                <div class="card">Card 2</div>
-            </div>
-            <div class="column">
-                <div class="card">Card 3</div>
-            </div>
-            <div class="column">
-                <div class="card">Card 4</div>
-            </div>
-</div> 
 
+            <div class = "mains">
+                <h3>Mains</h3>
+
+                <div class="row">
+                    <div class="column">
+                        <div class="card">
+                            <img src="images/pizza.jpg" width="100" height="100" alt="Pizza">
+                            <p><b><a href="recipes/pizza.php">Neopolitan Pizza</a></b></p>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 2</div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 3</div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 4</div>
+                    </div>
+                </div> 
+            </div>
+            
+            <div class = "desserts">
+                <h3>Desserts</h3>
+
+                <div class="row">
+                    <div class="column">
+                        <div class="card">
+                            <img src="images/baileys_cheesecake.png" width="100" height="100" alt="Baileys cheesecake">
+                            <p><b><a href="recipes/baileys_cheesecake.php">Baileys Cheesecake</a></b></p>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 2</div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 3</div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 4</div>
+                    </div>
+                </div> 
+            </div>
+
+            <div class = "sides">
+                <h3>Sides</h3>
+
+                <div class="row">
+                    <div class="column">
+                        <div class="card">
+                            <img src="images/burger_sauce.png" width="100" height="100" alt="Burger Sauce">
+                            <p><b><a href="recipes/burger_sauce.php">Burger Sauce</a></b></p>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 2</div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 3</div>
+                    </div>
+                    <div class="column">
+                        <div class="card">Card 4</div>
+                    </div>
+                </div> 
+
+            </div>
         </div>
     </div>
 
